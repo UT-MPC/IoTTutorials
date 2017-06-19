bb4e3a182c320778434c611bb94613a3dd6d0b5c
chr1s77 <christine.julien@gmail.com>
2017-06-19 08:52:13 -0500

    adding the Android side of the BT socket

---
+++ bluetoothSocket/BluetoothTestApp/app/src/main/java/com/example/bluetoothtestapp/MainActivity.java
@@ -0,0 +1,145 @@
+package com.example.bluetoothtestapp;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+    final Handler handler = new Handler();
+    BluetoothSocket mmSocket;
+    BluetoothDevice mmDevice = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(!mBluetoothAdapter.isEnabled()){
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth, 0);
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if(pairedDevices.size() > 0) {
+            for(BluetoothDevice device : pairedDevices) {
+                if(device.getName().equals("ChristinesRPi")) { //Note, you will need to change this to match the name of your device{
+                    mmDevice = device;
+                    break;
+                }
+            }
+        }
+
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener (new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on temp button click
+                (new Thread(new WorkerThread("Hello?"))).start();
+            }
+        });
+
+    }
+
+    public void sendBtMsg(String msg){
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); //Standard SerialPortService ID
+        try {
+            mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+            if (!mmSocket.isConnected()){
+                mmSocket.connect();
+            }
+            OutputStream mmOutputStream = mmSocket.getOutputStream();
+            mmOutputStream.write(msg.getBytes());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    final class WorkerThread implements Runnable {
+
+        private String btMsg;
+
+        public WorkerThread(String msg) {
+            btMsg = msg;
+        }
+
+        public void run() {
+            byte delimiter = 33;
+            int readBufferPosition = 0;
+            final TextView btResult = (TextView) findViewById(R.id.textView);
+
+            sendBtMsg(btMsg);
+
+            while(!Thread.currentThread().isInterrupted()) {
+
+                int bytesAvailable;
+                boolean workDone = false;
+
+                try {
+                    final InputStream mmInputStream;
+                    mmInputStream = mmSocket.getInputStream();
+                    bytesAvailable = mmInputStream.available();
+                    if(bytesAvailable > 0) {
+                        byte[] packetBytes = new byte[bytesAvailable];
+                        byte[] readBuffer = new byte[1024];
+                        mmInputStream.read(packetBytes);
+
+                        for(int i=0;i<bytesAvailable;i++) {
+                            byte b = packetBytes[i];
+                            if(b == delimiter) {
+                                byte[] encodedBytes = new byte[readBufferPosition];
+                                System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                final String data = new String(encodedBytes, "US-ASCII");
+                                readBufferPosition = 0;
+
+                                //The variable data now contains our full command
+                                handler.post(new Runnable() {
+                                    public void run() {
+                                        btResult.setText(data);
+                                    }
+                                });
+
+                                workDone = true;
+                                break;
+
+
+                            }
+                            else {
+                                readBuffer[readBufferPosition++] = b;
+                            }
+                        }
+
+                        if (workDone){
+                            mmSocket.close();
+                            break;
+                        }
+
+                    }
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+
+
+}
+
+
